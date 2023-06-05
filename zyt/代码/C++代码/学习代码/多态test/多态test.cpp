62f57cafbeead12965d5c00d198fe4cee24a1cd5
Ruiren1 <130643641+Ruiren1@users.noreply.github.com>
2023-06-05 20:05:04 +0800

    多态学习代码

---
+++ zyt/代码/C++代码/学习代码/多态test/多态test.cpp
@@ -0,0 +1,127 @@
+﻿
+#include <iostream>
+using namespace std;
+//class Person {
+//public:
+//    virtual~Person() { cout << "~Person()" << endl; }
+//};
+//class Student : public Person {
+//public:
+//    virtual ~Student() { cout << "~Student()" << endl; }
+//};
+//// 只有派生类Student的析构函数重写了Person的析构函数，下面的delete对象调用析构函
+//int main()
+//{
+//    Person* p1 = new Person;
+//    Person* p2 = new Student;
+//    delete p1;
+//    delete p2;
+//    return 0;
+//}
+//
+//
+//class Car
+//{
+//public:
+//	virtual void Drive() final {}
+//};
+//class Benz :public Car
+//{
+//public:
+//	virtual void Drive() { cout << "Benz-舒适" << endl; }
+//
+
+//class Car
+//{
+//public:
+//	virtual void Drive() {}
+//};
+//class Benz :public Car 
+//{
+//public:
+//	virtual void Drive() override { cout << "Benz-舒适" << endl; }
+//};
+
+//class Car
+//{
+//public:
+//	virtual void Drive() = 0;
+//};
+//class Benz :public Car
+//{
+//public:
+//	virtual void Drive()
+//	{
+//		cout << "Benz-舒适" << endl;
+//	}
+//};
+//class BMW :public Car
+//{
+//public:
+//	void Drive()
+//	{
+//		cout << "BMW-操控" << endl;
+//	}
+//};
+//void Test()
+//{
+//	Car A;
+//	Car* pBenz = new Benz;
+//	pBenz->Drive();
+//	Car* pBMW = new BMW;
+//	pBMW->Drive();
+//}
+
+//class Base
+//{
+//public:
+//	virtual void Func1()
+//	{
+//		cout << "Func1()" << endl;
+//	}
+//private:
+//	int _b = 1;
+//};
+//
+//int main()
+//{
+//	Base b;
+//	cout << sizeof(b) << endl;
+//	return 0;
+//}
+
+
+class Base
+{
+public:
+	virtual void Func1()
+	{
+		cout << "Base::Func1()" << endl;
+	}
+	virtual void Func2()
+	{
+		cout << "Base::Func2()" << endl;
+	}
+	void Func3()
+	{
+		cout << "Base::Func3()" << endl;
+	}
+private:
+	int _b = 1;
+};
+class Derive : public Base
+{
+public:
+	virtual void Func1()
+	{
+		cout << "Derive::Func1()" << endl;
+	}
+private:
+	int _d = 2;
+};
+int main()
+{
+	Base b;
+	Derive d;
+	return 0;
+}
